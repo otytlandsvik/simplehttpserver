0ffdc2d9c934667e1b2d4e8daaa185b1476c57b3
Ole Tytlandsvik <ole.tytlandsvik@gmail.com>
2022-03-16 22:29:50 +0100

    initial commit

---
+++ server.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+	"strconv"
+	"sync"
+)
+
+/* Globals */
+var counter int
+var mutex = &sync.Mutex{}
+
+/* echo path */
+func echo(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "hello, %q", html.EscapeString(r.URL.RawPath))
+}
+
+/* Increment counter and display on page */
+func incrementCounter(w http.ResponseWriter, r *http.Request) {
+	mutex.Lock()
+	counter++
+	fmt.Fprintf(w, strconv.Itoa(counter))
+	mutex.Unlock()
+}
+
+func main() {
+
+	/* General handler */
+	http.HandleFunc("/", echo)
+
+	/* Handler for /hi */
+	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hi")
+	})
+
+	/* Handler for incrementor */
+	http.HandleFunc("/increment", incrementCounter)
+
+	/* Run http server */
+	log.Fatal(http.ListenAndServe(":8081", nil))
+
+}
